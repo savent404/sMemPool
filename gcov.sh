91f7aa4545e68d4769e4be18dfa7404a206a13e9
savent <savent_gate@outlook.com>
2020-11-24 17:14:00 +0800

    enhance gcov

--- gcov.sh
+++ gcov.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DENABLE_TEST=ON -DENABLE_GCOV=ON
+make -C build
+
+mkdir -p build/gcov
+
+./build/test_pool
+mkdir -p build/gcov/pool
 gcov build/CMakeFiles/test_pool.dir/test/pool_test.cxx.gcno
-# gcov build/CMakeFiles/sMemPool.dir/test/list_test.cxx.gcno
+gcov build/CMakeFiles/sMemPool.dir/source/list_allocator.cxx.gcno
+mv *.gcov build/gcov/pool
+
+./build/test_list
+mkdir -p build/gcov/list_allocator
+gcov build/CMakeFiles/sMemPool.dir/source/list_allocator.cxx.gcno
+mv *.gcov build/gcov/list_allocator
